d902c5c19c4094f78f4496816b20f8d6eea69776
Lucas Augusto <lucas.augusto5061@gmail.com>
2019-01-11 23:52:45 -0200

    adicionado arquivo de serviço para o billing cycle

---
+++ src/api/billingCycle/billingCycleService.js
@@ -0,0 +1,6 @@
+const billingCycle = require('./billingCycle')
+
+billingCycle.methods(['get', 'post', 'put', 'delete'])
+billingCycle.updateOptions({ new: true, runValidators: true })
+
+module.exports = billingCycle
