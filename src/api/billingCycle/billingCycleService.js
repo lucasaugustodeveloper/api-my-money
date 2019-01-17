5169dbdb6284936c9707bd882f3140d1b57350d9
Lucas Augusto <lucas.augusto5061@gmail.com>
2019-01-17 21:29:27 -0200

    adicionado middleware de tratamento de erros

--- src/api/billingCycle/billingCycleService.js
+++ src/api/billingCycle/billingCycleService.js
@@ -1,7 +1,11 @@
 const billingCycle = require('./billingCycle')
+const errorHandle = require('../../common/errorHandle')
 
 billingCycle.methods(['get', 'post', 'put', 'delete'])
 billingCycle.updateOptions({ new: true, runValidators: true })
+billingCycle
+  .after('post', errorHandle)
+  .after('put', errorHandle)
 
 billingCycle.route('count', (req, res, next) => {
   billingCycle.count((error, value) => {
