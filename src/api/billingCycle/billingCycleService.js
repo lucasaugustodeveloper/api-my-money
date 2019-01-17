74cea967b2e363f0f52f08ebb7ff5d7d8957f9f9
Lucas Augusto <lucas.augusto5061@gmail.com>
2019-01-17 21:16:15 -0200

    adicionado rota de count de registros no banco

--- src/api/billingCycle/billingCycleService.js
+++ src/api/billingCycle/billingCycleService.js
@@ -3,4 +3,16 @@ const billingCycle = require('./billingCycle')
 billingCycle.methods(['get', 'post', 'put', 'delete'])
 billingCycle.updateOptions({ new: true, runValidators: true })
 
+billingCycle.route('count', (req, res, next) => {
+  billingCycle.count((error, value) => {
+    if (error) {
+      return res.status(500).json({ errors: [error] })
+    }
+
+    if (!error) {
+      res.json({ value })
+    }
+  })
+})
+
 module.exports = billingCycle
