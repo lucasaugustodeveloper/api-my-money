6e7025331127d257e20e2d4fbeaa945ff389e4dd
Lucas Augusto <lucas.augusto5061@gmail.com>
2019-01-17 22:18:40 -0200

    adicionado rota de sumario para trazer todos os créditos os débitos

--- src/api/billingCycle/billingCycleService.js
+++ src/api/billingCycle/billingCycleService.js
@@ -19,4 +19,20 @@ billingCycle.route('count', (req, res, next) => {
   })
 })
 
+billingCycle.route('summary', (req, res, next) => {
+  billingCycle.aggregate([{
+    $project: { credit: { $sum: "$credits.value" }, debt: { $sum: "$debts.value" } }
+  }, {
+    $group: { _id: null, credit: { $sum: "$credit" }, debt: { $sum: "$debt" } }
+  }, {
+    $project: { _id: 0, credit: 1, debt: 1 }
+  }]).exec((error, result) => {
+    if (error) {
+      return res.status(500).json({ errors: [error] })
+    }
+
+    res.json(result[0] || { credit: 0, debt: 0 })
+  })
+})
+
 module.exports = billingCycle
