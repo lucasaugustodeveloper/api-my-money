d8a8e5c74c13628a0ebbb799d84a0c2e7aae2ccf
Lucas Augusto <lucas.augusto5061@gmail.com>
2019-07-27 15:09:30 -0300

    Add default port without case environment PORT not defined

--- src/config/server.js
+++ src/config/server.js
@@ -4,7 +4,7 @@ const express = require('express')
 const server = express()
 const cors = require('./cors')
 
-const port = process.env.PORT
+const port = process.env.PORT || 80
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
